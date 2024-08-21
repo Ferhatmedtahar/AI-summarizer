ee633b2cbd582e09ab172740c4bf0b0b21c978b8
ferhat <ferhattahar46@gmail.com>
2024-08-21 19:19:26 +0100

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import copy from "./copy.svg";
+import linkIcon from "./link.svg";
+import loader from "./loader.svg";
+import logoApp from "./logoApp.svg";
+import tick from "./tick.svg";
+
+export { copy, linkIcon, loader, logoApp, tick };
